aab042fb9bf8e7a640b6c89f5eccb555582789a7
joe <123@qq.com>
2020-11-05 14:13:05 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/test/Hello.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author xxh
+ * 2020-11-05 14:01
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello!");
+    }
+}
